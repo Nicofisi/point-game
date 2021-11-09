9f11bc26e150b994bcf15cfe46a7ecf7c4a12b99
Nicofisi <m.lomiak@gmail.com>
2021-11-09 03:37:21 +0100

    Fix mobile styles (2.4.1)

--- src/components/MainRoute.js
+++ src/components/MainRoute.js
@@ -58,7 +58,7 @@ export default function MainRoute ({ sectionData }) {
         </main>
       </div>
       <footer className={styles.footer}>
-        <div>Copyright Nicofisi 2021 | Wersja 2.4.0</div>
+        <div>Copyright Nicofisi 2021 | Wersja 2.4.1</div>
       </footer>
     </div>
   )
